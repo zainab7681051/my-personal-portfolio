451a26e537ad73e4d2160b705b95c458a6f5e3f3
zainab7681051 <zainab7681051@gmail.com>
2022-10-29 23:11:12 +0300

    m

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,34 +1,3 @@
-<script setup>
-  import axios from 'axios'
-  import {ref} from 'vue'
-  import emailjs from '@emailjs/browser'
-  let name=ref(null)
-  let email=ref(null)
-  let message=ref(null)
-
-  const send=async ()=>{
-    try {
-      // statements
-      const form={
-        from_name:name.value,
-        from_email:email.value,
-        message:message.value,
-      }
-        const data={service_id:'service_k5e6q6j',
-        template_id:'template_7cjnib2',
-        public_key:'2oi10VTZzujXopEAa',
-      }
-      await emailjs.send(data.service_id, data.template_id,form,data.public_key)
-
-      console.log(data)
-    } catch(e) {
-      // statements
-      console.log(e);
-    }
-  }
-
-</script>
-
 <template>
     <section id="contact" class="px-10 pt-20 bg-back-900">
           <div class="container">
@@ -42,22 +11,126 @@
             <form>
               <div class="w-full lg:mx-auto lg:w-2/3">
                 <div class="mb-8 w-full px-4">
-                  <label for="name" class="text-base font-bold text-primary">Nama</label>
-                  <input type="text" v-model="name" class="w-full rounded-md bg-slate-200 p-3 text-dark focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary" />
+                  <label for="name" class="text-base font-bold text-primary">Nama
+                    </label>
+                  <input type="text" v-model="name"
+                  required
+                  class="w-full rounded-md bg-slate-200 p-3 text-dark focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary" />
                 </div>
                 <div class="mb-8 w-full px-4">
                   <label for="email" class="text-base font-bold text-primary">Email</label>
-                  <input type="email" v-model="email" class="w-full rounded-md bg-slate-200 p-3 text-dark focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary" />
+                  <input v-model="email"
+                  required
+                  type="email"
+                  class="w-full rounded-md bg-slate-200 p-3 text-dark focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary"
+                  />
                 </div>
                 <div class="mb-8 w-full px-4">
                   <label for="message" class="text-base font-bold text-primary">Message</label>
-                  <textarea type="email" v-model="message" class="h-32 w-full rounded-md bg-slate-200 p-3 text-dark focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary"></textarea>
+                  <textarea v-model="message"
+                  required 
+                  class="h-32 w-full rounded-md bg-slate-200 p-3 text-dark focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary"
+                  ></textarea>
                 </div>
                 <div class="w-full px-4">
-                  <button class="w-full rounded-full bg-primary py-3 px-8 text-base font-semibold text-white transition duration-500 hover:opacity-80 hover:shadow-lg" @click.prevent="send()">Send</button>
+                  <button id="btn" class="w-full rounded-full bg-primary py-3 px-8 text-base font-semibold text-white transition duration-500 hover:opacity-80 hover:shadow-lg" @click.prevent="send()">Send</button>
+                  <span v-for="er in errors" :key="er" class="text-red-400 md:text-lg m-2 block text-center">{{er}}</span>
+                  <span v-if="sent" class="text-slate-100 md:text-lg m-2 block text-center">
+                  🎉 message was succesfully sent! 🎉</span>
                 </div>
               </div>
             </form>
           </div>
         </section>
 </template>
+
+<script>
+import emailjs from '@emailjs/browser'
+import useVuelidate from '@vuelidate/core'
+import { required, email, minLength } from '@vuelidate/validators'
+export default{
+  data(){
+    return {
+      v$:useVuelidate(),
+      name:'',
+      email:'',
+      message:'',
+      errors:{
+        name:'',
+        email:'',
+        message:'',
+        error:''
+      },
+      sent:false,
+    }
+  },
+
+  methods:{
+    async send(){
+      const element = document.getElementById('btn')
+      element.classList.add('cursor-progress');
+      if (this.vali()){
+       try {
+          // statements
+          this.errors={}
+          this.sent=false
+          const form = {
+            from_name: this.name,
+            from_email: this.email,
+            message: this.message,
+          }
+          const data = {
+            service_id: import.meta.env.VITE_SERVICE_ID,
+            template_id: import.meta.env.VITE_TEMPLATE_ID,
+            public_key: import.meta.env.VITE_PUBLIC_KEY,
+          }
+          const res=await emailjs.send(data.service_id, data.template_id, form, data.public_key)
+          if(res.status==200)
+          {
+            this.sent=true
+            this.name=''
+            this.email=''
+            this.message=''
+          }
+        } catch (e) {
+          // statements
+          console.log(e);
+          if(res.status==400)
+          {
+            this.sent=false
+            this.errors.error="an error occured. the email was not send."
+          }
+        }finally{
+          element.classList.remove('cursor-progress');
+        }
+      }else{
+        element.classList.remove('cursor-progress');
+      }
+    },
+    vali(){
+      if (this.name.length===0){
+        this.errors.name='name is required'; 
+        return false
+      }else{
+        this.errors.name='';
+      }
+      if(this.email.length===0){
+        this.errors.email='email is required'; 
+        return false
+      }else if(!this.email.includes('@gmail.com')){
+        this.errors.email='Email must be valid and in Gmail form'
+        return false;
+      }else{
+        this.errors.email=''
+      }
+      if (this.message.length===0){
+        this.errors.message='message is required'; 
+        return false
+      }else{
+        this.errors.message='';
+      }
+      return true
+    },
+  },
+}
+</script>
--- src/components/home.vue
+++ src/components/home.vue
@@ -4,13 +4,9 @@
           <div class="w-full px-4 items-center text-center ">
             <h1 class="text-base font-semibold text-primary md:text-xl">Hello there 👋, I am 
             <span class="mt-1 block text-4xl font-bold text-dark text-white lg:text-5xl">Zainab</span></h1>
-            <h2 class="mb-5 mt-3 text-lg font-medium text-secondary lg:text-2xl">web developer</h2>
-            <p class="mb-10 font-medium leading-relaxed text-secondary">And Welcome to my Portfolio!
-              <br> As a full stack web developer, my goal is to learn new skills and put those skills to use in the 
-            <span class="font-bold text-white">real world</span> 
-              <span class="text-2xl">
-                🌍
-              </span></p>
+            <h2 class="mb-5 mt-3 text-lg font-medium text-secondary lg:text-2xl">And Welcome to my Portfolio!</h2>
+            <p class="mb-10 font-medium leading-relaxed text-secondary">
+            I am a full stack web developer who can work on a project from beginning to end using the best frameworks and tools to achieve the best results</p>
           </div>
       </div>
     </section>
