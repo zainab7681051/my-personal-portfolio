98d3fecab130b7975decef9081c4715c067fb186
zainab7681051 <zainab7681051@gmail.com>
2022-12-13 19:24:06 +0300

    m

---
+++ src/components/project.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+defineProps({
+    title:String,
+    description:String,
+    imgSrc:String,
+    githubUrl:String,
+    appUrl:String
+})
+</script>
+
+<template>
+	<div class="mb-12 p-4 md:w-1/2">
+		<div class="overflow-hidden rounded-md">
+		  <img :src=imgSrc :alt=title width="900"/>
+		</div>
+		<h3 class="mt-5 mb-3 text-xl font-semibold text-white">
+		{{title}}</h3>
+		<p class="text-base font-medium text-secondary">{{description}}</p>
+		<div class="flex">
+		  <a :href=githubUrl
+		    target="_blank" title="view code on github">
+		    <icon icon="akar-icons:github-fill"
+		    width="24" height="24" 
+		    class="h-10 w-10 
+		    relative flex items-center 
+		    justify-center
+		    mt-2 mb-2 mx-auto
+		    p-2
+		    bg-back-900
+		    text-primary 
+		    hover:bg-primary
+		    hover:text-back-600
+		    rounded-xl 
+		    transition-all cursor-pointer
+		    duration-300 ease-linear;"/>
+		  </a>
+		  <a :href=appUrl
+		    target="_blank" title="visit this app">
+		    <icon icon="codicon:live-share"
+		    width="24" height="24" 
+		    class="h-10 w-10 
+		    relative flex items-center 
+		    justify-center
+		    mt-2 mb-2 mx-auto
+		    p-2
+		    bg-back-900
+		    text-primary 
+		    hover:bg-primary
+		    hover:text-back-600
+		    rounded-xl 
+		    transition-all cursor-pointer
+		    duration-300 ease-linear;"/>
+		  </a>
+		</div>
+	</div>
+</template>
--- src/components/projects.vue
+++ src/components/projects.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { Icon } from '@iconify/vue';
+import project from "./project.vue"
 </script>
 
 <template>
@@ -8,130 +8,34 @@ import { Icon } from '@iconify/vue';
             <div class="w-full px-4">
               <div class="mx-auto mb-16 max-w-xl text-center">
                 <h4 class="mb-2 text-lg font-semibold text-primary">Projects</h4>
-                <h2 class="mb-4 text-3xl font-bold text-white sm:text-4xl lg:text-5xl">Finished Works</h2><!-- 
-                <p class="text-md font-medium text-secondary md:text-lg">
-                  here you can find what I have worked on in the past.
-                </p> -->
+                <h2 class="mb-4 text-3xl font-bold text-white sm:text-4xl lg:text-5xl">Finished Works</h2>
               </div>
             </div>
             <div class="flex w-full flex-wrap justify-center px-4 xl:mx-auto xl:w-10/12">
-              <div class="mb-12 p-4 md:w-1/2">
-                <div class="overflow-hidden rounded-md">
-                  <img src="../assets/free-documentaries.png" alt="free-documentaries.png" width="900"/>
-                </div>
-                <h3 class="mt-5 mb-3 text-xl font-semibold text-white">Free-Documentaries</h3>
-                <p class="text-base font-medium text-secondary">A web App for viewing free documentaries fetched from various YouTube Channels.</p>
-                <div class="flex">
-                  <a href="https://github.com/zainab7681051/free-documentaries"
-                    target="_blank" title="view code on github">
-                    <icon icon="akar-icons:github-fill"
-                    width="24" height="24" 
-                    class="h-10 w-10 
-                    relative flex items-center 
-                    justify-center
-                    mt-2 mb-2 mx-auto
-                    p-2
-                    bg-back-900
-                    text-primary 
-                    hover:bg-primary
-                    hover:text-back-600
-                    rounded-xl 
-                    transition-all cursor-pointer
-                    duration-300 ease-linear;"/>
-                  </a>
-                  <a href="https://free-documentaries.onrender.com/"
-                    target="_blank" title="visit this app">
-                    <icon icon="codicon:live-share"
-                    width="24" height="24" 
-                    class="h-10 w-10 
-                    relative flex items-center 
-                    justify-center
-                    mt-2 mb-2 mx-auto
-                    p-2
-                    bg-back-900
-                    text-primary 
-                    hover:bg-primary
-                    hover:text-back-600
-                    rounded-xl 
-                    transition-all cursor-pointer
-                    duration-300 ease-linear;"/>
-                  </a>
-              </div>
-              </div>
               
-              <div class="mb-12 p-4 md:w-1/2">
-                <div class="overflow-hidden rounded-md shadow-md">
-                  <img src="../assets/pizza-delivery.png" alt="pizza-delivery.png" width="900" />
+            <project title="Image-Genreator" 
+            description="A node express app for generating input-based images using openAI technology." 
+            imgSrc="open-ai-image-generator-api.png"
+            githubUrl="https://github.com/zainab7681051/image-generation-app-with-openAI/" 
+            appUrl="https://openai-images.onrender.com/"></project>
 
-                </div>
-                <h3 class="mt-5 mb-3 text-xl font-semibold text-white">Pizza-Delivery</h3>
-                <p class="text-base font-medium text-secondary">A wep app for pizzerias. A user can log in and order pizza's and their order is saved in the cloud database.</p>
+            <project title="Free-Documentaries" 
+            description="A web App for viewing free documentaries fetched from various YouTube Channels." 
+            imgSrc="free-documentaries.png"
+            githubUrl="https://github.com/zainab7681051/free-documentaries" 
+            appUrl="https://free-documentaries.onrender.com/"></project>
 
-                <div class="flex">
-                  <a href="https://github.com/zainab7681051/pizza-delivery"
-                    target="_blank" title="view code on github">
-                    <icon icon="akar-icons:github-fill"
-                    width="24" height="24" 
-                    class="h-10 w-10 
-                    relative flex items-center 
-                    justify-center
-                    mt-2 mb-2 mx-auto
-                    p-2
-                    bg-back-900
-                    text-primary 
-                    hover:bg-primary
-                    hover:text-back-600
-                    rounded-xl 
-                    transition-all cursor-pointer
-                    duration-300 ease-linear;"/>
-                  </a>
-                  <a href="https://pizza-delivery-5201d.web.app/"
-                    target="_blank" title="visit this app">
-                    <icon icon="codicon:live-share"
-                    width="24" height="24" 
-                    class="h-10 w-10 
-                    relative flex items-center 
-                    justify-center
-                    mt-2 mb-2 mx-auto
-                    p-2
-                    bg-back-900
-                    text-primary 
-                    hover:bg-primary
-                    hover:text-back-600
-                    rounded-xl 
-                    transition-all cursor-pointer
-                    duration-300 ease-linear;"/>
-                  </a>
-              </div>
-              </div>
+            <project title="Pizza-Delivery" 
+            description="A wep app for pizzerias. A user can log in and order pizza's and their order is saved in the cloud database." 
+            imgSrc="pizza-delivery.png"
+            githubUrl="https://github.com/zainab7681051/pizza-delivery" 
+            appUrl="https://pizza-delivery-5201d.web.app/"></project>
 
-              <div class="mb-12 p-4 md:w-1/2">
-                <div class="overflow-hidden rounded-md shadow-md">
-                  <img src="../assets/todolist.png" alt="todolist.png" width="900" />
-                </div>
-                <h3 class="mt-5 mb-3 text-xl font-semibold text-white">todo app</h3>
-                <p class="text-base font-medium text-secondary">simple todo app for keepig track of your tasks and objectives.</p>
-
-                <div class="flex">
-                  <a href="https://github.com/zainab7681051/simple-to-do-list-app"
-                    target="_blank" title="view code on github">
-                    <icon icon="akar-icons:github-fill"
-                    width="24" height="24" 
-                    class="h-10 w-10 
-                    relative flex items-center 
-                    justify-center
-                    mt-2 mb-2 mx-auto
-                    p-2
-                    bg-back-900
-                    text-primary 
-                    hover:bg-primary
-                    hover:text-back-600
-                    rounded-xl 
-                    transition-all cursor-pointer
-                    duration-300 ease-linear;"/>
-                  </a>
-              </div>
-              </div>
+            <project title="todo app" 
+            description="simple todo app for keepig track of your tasks and objectives." 
+            imgSrc="todolist.png"
+            githubUrl="https://github.com/zainab7681051/simple-to-do-list-app"
+            appUrl=""></project>
             </div>
           </div>
         </section>    
