621ce31d0b721be14176e2c616edfb2d4a3e9d2e
zainab7681051 <zainab7681051@gmail.com>
2023-01-21 20:49:55 +0300

    m

--- src/components/home.vue
+++ src/components/home.vue
@@ -4,8 +4,9 @@
           <div class="w-full px-4 items-center text-center ">
             <h1 class="
              text-base font-semibold text-primary text-2xl">Hello there 👋, I am 
-            <span class="mt-1 block text-4xl font-bold text-dark text-slate-100 lg:text-5xl
+            <span class="mt-1 block text-8xl font-bold text-dark text-slate-100
             text-shadow-pop-bl
+            outline-black
             ">Zainab</span></h1>
             <p class="mt-5 mb-10 font-medium leading-relaxed text-secondary">
             I am a full stack web developer and I can create web-based applications with captivating frontend UI, and operational backend code with maintainble databases</p>
