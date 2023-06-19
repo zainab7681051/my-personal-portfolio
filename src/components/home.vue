c1e2551f179203a0efc7cae138d4dc2c923b9760
zainab7681051 <zainab76810510@gmail.com>
2023-06-20 02:16:38 +0300

    modified

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,33 +1,41 @@
 <template>
-    <section class="px-10 bg-back-900">
-      <div class="container">
-          <div class="w-full px-4 items-center text-center ">
-            <h1 class="
-             text-base font-semibold text-primary text-2xl top-text greet">Hello there 👋, I am 
-            <span class="mt-1 block text-7xl md:text-8xl font-bold text-dark text-slate-100
-            text-shadow-pop-bl
-            outline-black mt-5
-            top-text name">Zainab</span></h1>
-            <p class="mt-5 mb-10 font-medium leading-relaxed text-secondary bottom-text">
-            I am a full stack web developer and I can create web-based applications with captivating frontend UI, and operational backend code with maintainble databases</p>
-          </div>
+  <section class="px-10 bg-back-900">
+    <div class="container">
+      <div class="w-full px-4 items-center text-center">
+        <h1
+          class="text-base font-semibold text-primary text-2xl top-text greet"
+        >
+          Hello there 👋, I am
+          <span
+            class="mt-1 block text-7xl md:text-8xl font-bold text-dark text-slate-100 text-shadow-pop-bl outline-black mt-5 top-text name"
+            >Zainab</span
+          >
+        </h1>
+        <p
+          class="mt-5 mb-10 font-medium leading-relaxed text-secondary bottom-text"
+        >
+          A software developer skilled in HTML, CSS, JavaScript, Node.js,
+          Vue.js, C#, .NET Core and more. I create efficient and engaging
+          web-based softwares and solutions, focusing on quality and user
+          experience. Let's collaborate and bring your ideas to life!
+        </p>
       </div>
-    </section>
+    </div>
+  </section>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
 <style scoped>
-    .top-text{
-      opacity: 0;
-      transform: translateY(-50vh);
-    }
-    .top-text.name{
-    -webkit-text-stroke: 2px #000;
-    }
-    .bottom-text{
-      opacity: 0;
-      transform: translateX(200vw);
-    }
+.top-text {
+  opacity: 0;
+  transform: translateY(-50vh);
+}
+.top-text.name {
+  -webkit-text-stroke: 2px #000;
+}
+.bottom-text {
+  opacity: 0;
+  transform: translateX(200vw);
+}
 </style>
